6d299a5213380c9638b3b60cb851b7b51949fe5a
jack <guangjian.cgj@alibaba-inc.com>
2017-04-11 17:50:20 +0800

    在client apk上监听socket端口，触发H5 Test WebView执行。

--- app/src/main/java/com/example/jack/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/jack/myapplication2/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     Button testButton;
@@ -21,14 +27,50 @@ public class MainActivity extends AppCompatActivity {
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, H5TestActivity.class);
-                intent.putExtra("testUrl", testUrl);
-                startActivity(intent);
+                runTest(testUrl);
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //创建服务端Socket
+                    ServerSocket serverSocket = new ServerSocket(8888);
+                    Socket socket;
+                    int read;
+                    String content;
+
+                    while ((socket = serverSocket.accept()) != null) {//当有客户端连接时进入
+                        InputStream is = socket.getInputStream();
+                        OutputStream os = socket.getOutputStream();
+                        content = "";
+                        while ((read = is.read()) != -1) {
+                            System.out.print((char) read);
+                            content += (char) read;
+                        }
+                        os.write("ok".getBytes());
+                        os.close();
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
     }
 
+    /**
+     * 在当前机器的apk上执行H5测试
+     *
+     * @param testUrl
+     */
+    private void runTest(String testUrl) {
+        Intent intent = new Intent(MainActivity.this, H5TestActivity.class);
+        intent.putExtra("testUrl", testUrl);
+        startActivity(intent);
+    }
 
 
 }
